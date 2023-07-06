25fd86824439323e405979305d1e936750d01f5d
ImExtraPolite <jaleel1666@gmail.com>
2023-07-05 23:49:37 -0400

    almost done

--- javascript.js
+++ javascript.js
@@ -6,21 +6,31 @@ const check = document.querySelector(".check");
 let randNum = Math.floor(Math.random() * 100) + 1; 
 const userGuess = document.querySelector(".userGuess");
 const p1 = document.querySelector(".bottomLeft p:first-child");
-
+let down = 10;
+const reset = document.querySelector(".class");
 
 check.addEventListener("click", () => {
-    if (numScore.value < 0 || numScore.value > 100) {
-        p1.textContent = "Out of bounds";
-    } else if (randNum == numScore.value) {
-        p1.textContent = "Correct";
-
-        
-    } else if (numScore.value < randNum) {
-        p1.textContent = "Too low";
-    } else if (numScore.value > randNum) {
-        p1.textContent = "Too high";
+    if (down < 1) {
+        p1.textContent = "You lose";
+    } else {
+        if (numScore.value < 0 || numScore.value > 100) {
+            p1.textContent = "Out of bounds";
+        } else if (randNum == numScore.value) {
+            p1.textContent = "Correct";
+            highscore.textContent = score.textContent;
+        } else if (numScore.value < randNum) {
+            p1.textContent = "Too low";
+            down = down - 1; 
+            score.textContent = down;
+        } else if (numScore.value > randNum) {
+            p1.textContent = "Too high";
+            down = down - 1; 
+            score.textContent = down;
+        }
     }
 });
+
+
 
 
 
